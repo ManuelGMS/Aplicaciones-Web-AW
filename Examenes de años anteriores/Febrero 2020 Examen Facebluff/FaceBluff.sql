e6b6ba55b56f7705ddeb9055b57db464196ced30
ManuelGMS <guerre1510@gmail.com>
2020-01-26 23:42:06 +0100

    Aplicaciones Web

--- Examenes de años anteriores/Febrero 2020 Examen Facebluff/FaceBluff.sql
+++ Examenes de años anteriores/Febrero 2020 Examen Facebluff/FaceBluff.sql
@@ -1,21 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 24-01-2020 a las 20:09:55
--- Versión del servidor: 10.3.16-MariaDB
--- Versión de PHP: 7.3.7
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `facebluff`
